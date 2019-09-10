152f0b1423abd85fa5824d1c23e46892e1c9ab57
John Dee <ekstrandcaleb@gmail.com>
2019-09-10 15:21:20 -0600

    Fixed errors

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+
+
+                // Creates an array that lists out all of the options
+                var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            
+                var wins = 0;
+                var losses = 0;
+                var userGuess = "";
+                // Randomly chooses a choice from the array. Computer's guess.
+                var computerGuess = ""
+            
+                var userChoiceText = document.getElementById("userChoice-text");
+                var computerChoiceText = document.getElementById("computerChoice-text");
+                var guessesLeft = 10
+                var directionText = document.getElementById("directions-text")
+                var winText = document.getElementById("wins")
+                console.log(winText)
+                var loseText = document.getElementById("loses")
+                function reset () {
+                    //reset global values
+                }
+                function winCheck() {
+
+                    // If letters match user wins
+                    if (userGuess === computerGuess) {
+                        wins++;
+                        winText.textContent = wins;
+                        
+                      } else if (userGuess !== computerGuess) {
+                          console.log("false")
+                        guessesLeft--;
+                        loseText.textContent = losses;
+                         /*
+                         if guessesLeft = 0 run reset function
+                         
+                         
+                         
+                         */
+                      // user guess
+                      userChoiceText.textContent = userGuess;
+                      computerChoiceText.textContent = computerGuess;
+                  }
+                }
+                // This is run whenever the user presses a key.
+                document.onkeyup = function (event) {
+                    userGuess = event.key;
+                     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                  // Determines which key was pressed.
+                  winCheck()
+                  
+                  
+            
+            
+                };
+            
+            
+        
